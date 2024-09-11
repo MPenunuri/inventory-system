900fabfa40b650896d31a4a2c2a702d19d02bff7
MPenunuri <manuel.penunuri.ram@gmail.com>
2024-09-11 13:51:22 -0600

    Set up CurrencyPersistencePort and refactor CurrencyRepositoryImpl accordingly

---
+++ src/main/java/com/mapera/inventory_system/application/port/outbound/CurrencyPersistencePort.java
@@ -0,0 +1,16 @@
+package com.mapera.inventory_system.application.port.outbound;
+
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.entity.CurrencyEntity;
+
+import reactor.core.publisher.Mono;
+
+public interface CurrencyPersistencePort {
+
+    public Mono<CurrencyEntity> registerCurrency(String name);
+
+    public Mono<String> getCurrencyNameById(Long currencyId);
+
+    public Mono<CurrencyEntity> renameCurrency(Long currencyId, String name);
+
+    public Mono<Void> deleteCurrency(Long currencyId);
+}
--- src/main/java/com/mapera/inventory_system/infrastructure/adapter/outbound/persistence/repository/currency/CurrencyRepositoryImpl.java
+++ src/main/java/com/mapera/inventory_system/infrastructure/adapter/outbound/persistence/repository/currency/CurrencyRepositoryImpl.java
@@ -4,15 +4,23 @@ package com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.mapera.inventory_system.application.port.outbound.CurrencyPersistencePort;
 import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.entity.CurrencyEntity;
 
 import reactor.core.publisher.Mono;
 
 @Repository
-public class CurrencyRepositoryImpl implements CurrencyRepositoryCustom {
+public class CurrencyRepositoryImpl implements CurrencyRepositoryCustom, CurrencyPersistencePort {
 
     @Autowired
     CurrencyCrudRepository currencyCrudRepository;
+
+    @Override
+    public Mono<CurrencyEntity> registerCurrency(String name) {
+        CurrencyEntity currencyEntity = new CurrencyEntity();
+        currencyEntity.setName(name);
+        return currencyCrudRepository.save(currencyEntity);
+    }
 
     @Override
     public Mono<CurrencyEntity> renameCurrency(Long currencyId, String name) {
@@ -20,6 +28,16 @@ public class CurrencyRepositoryImpl implements CurrencyRepositoryCustom {
             c.setName(name);
             return currencyCrudRepository.save(c);
         });
+    }
+
+    @Override
+    public Mono<String> getCurrencyNameById(Long currencyId) {
+        return currencyCrudRepository.findById(currencyId).map(c -> c.getName());
+    }
+
+    @Override
+    public Mono<Void> deleteCurrency(Long currencyId) {
+        return currencyCrudRepository.deleteById(currencyId);
     }
 
 }
