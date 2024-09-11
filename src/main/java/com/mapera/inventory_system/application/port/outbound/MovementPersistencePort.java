1a384c4faa776a5c7d47da609a4bd82b2a0f2d8d
MPenunuri <manuel.penunuri.ram@gmail.com>
2024-09-11 12:23:09 -0600

    Set up MovementPersistencePort and refactor MovementRepositoryImpl accordingly

---
+++ src/main/java/com/mapera/inventory_system/application/port/outbound/MovementPersistencePort.java
@@ -0,0 +1,121 @@
+package com.mapera.inventory_system.application.port.outbound;
+
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.AcquisitionDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.AverageCostProductDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.AverageSellProductDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.CustomerReturnDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.EntryMovementDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.OutputMovementDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.ProductionDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.SaleDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.StandardMovementDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.SupplierReturnDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.TransferMovementDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.entity.MovementEntity;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+public interface MovementPersistencePort {
+
+    Flux<StandardMovementDTO> getMovements();
+
+    Flux<EntryMovementDTO> getEntries();
+
+    Flux<OutputMovementDTO> getOutputs();
+
+    Flux<TransferMovementDTO> getTransfers();
+
+    Flux<AcquisitionDTO> getAcquisitions();
+
+    Flux<CustomerReturnDTO> getCustomerReturns();
+
+    Flux<EntryMovementDTO> getEntryInventoryAdjustments();
+
+    Flux<ProductionDTO> getProductions();
+
+    Flux<SaleDTO> getSales();
+
+    Flux<SupplierReturnDTO> getSupplierReturns();
+
+    Flux<OutputMovementDTO> getOutputInventoryAdjustments();
+
+    Flux<OutputMovementDTO> getInternalConsumptionMovements();
+
+    Flux<StandardMovementDTO> getMovementsByProductId(Long productId);
+
+    Flux<AcquisitionDTO> getAcquisitionsBySupplierId(Long supplierId);
+
+    Flux<AcquisitionDTO> findAcquisitionsByCostAndYear(Long currencyId, double minCost, double maxCost,
+            int fromYear, int toYear);
+
+    Flux<AverageCostProductDTO> getAvgUnitCostByAcquisition(Long productId, Long currencyId,
+            int fromYear, int toYear);
+
+    Flux<AverageCostProductDTO> getAvgTotalCostByAcquisition(Long productId, Long currencyId,
+            int fromYear, int toYear);
+
+    Flux<ProductionDTO> findProductionByCostAndYear(Long currencyId, double minCost, double maxCost,
+            int fromYear, int toYear);
+
+    Flux<AverageCostProductDTO> getAvgUnitProductionCost(Long productId, Long currencyId,
+            int fromYear, int toYear);
+
+    Flux<AverageCostProductDTO> getAvgTotalProductionCost(Long productId, Long currencyId,
+            int fromYear, int toYear);
+
+    Flux<SaleDTO> findSalesByValueAndYear(Long currencyId, double minValue, double maxValue,
+            int fromYear, int toYear);
+
+    Flux<AverageSellProductDTO> getAvgUnitSellValue(Long productId, Long currencyId,
+            int fromYear, int toYear);
+
+    Flux<AverageSellProductDTO> getAvgTotalSellValue(Long productId, Long currencyId,
+            int fromYear, int toYear);
+
+    public Mono<MovementEntity> addAcquisitionEntryMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long supplierId, Long toLocationId, String transactionSubtype,
+            double transactionValue, Long transactionCurrencyId);
+
+    public Mono<MovementEntity> addCustomerReturnEntryMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long toLocationId, String transactionSubtype,
+            double transactionValue, Long transactionCurrencyId);
+
+    public Mono<MovementEntity> addInventoryAdjustmentEntryMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long toLocationId);
+
+    public Mono<MovementEntity> addProductionEntryMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long toLocationId, String transactionSubtype,
+            double transactionValue, Long transactionCurrencyId);
+
+    public Mono<MovementEntity> addSalesOutputMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long fromLocationId, String transactionSubtype,
+            double transactionValue, Long transactionCurrencyId);
+
+    public Mono<MovementEntity> addSupplierReturnOutputMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long supplierId, Long fromLocationId,
+            String transactionSubtype, double transactionValue, Long transactionCurrencyId);
+
+    public Mono<MovementEntity> addInventoryAdjustmentOutputMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long fromLocationId);
+
+    public Mono<MovementEntity> addInternalConsumptionOutputMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long fromLocationId);
+
+    public Mono<MovementEntity> addTransferMovement(
+            Long productId, LocalDateTime dateTime, String reason, String comment,
+            int quantity, Long fromLocationId, Long toLocationId);
+
+    public Mono<Boolean> cancelMovementById(Long movementId);
+
+}
--- src/main/java/com/mapera/inventory_system/infrastructure/adapter/outbound/persistence/repository/movement/MovementRepositoryImpl.java
+++ src/main/java/com/mapera/inventory_system/infrastructure/adapter/outbound/persistence/repository/movement/MovementRepositoryImpl.java
@@ -5,6 +5,18 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.mapera.inventory_system.application.port.outbound.MovementPersistencePort;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.AcquisitionDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.AverageCostProductDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.AverageSellProductDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.CustomerReturnDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.EntryMovementDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.OutputMovementDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.ProductionDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.SaleDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.StandardMovementDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.SupplierReturnDTO;
+import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.dto.movement.TransferMovementDTO;
 import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.entity.MovementEntity;
 import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.repository.movement.custom_method.AddAcquisitionEntryMovement;
 import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.repository.movement.custom_method.AddCustomerReturnEntryMovement;
@@ -18,120 +30,254 @@ import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.r
 import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.repository.movement.custom_method.CancelMovementById;
 import com.mapera.inventory_system.infrastructure.adapter.outbound.persistence.repository.stock.StockRepository;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
-public class MovementRepositoryImpl implements MovementRepositoryCustom {
+public class MovementRepositoryImpl implements MovementRepositoryCustom, MovementPersistencePort {
 
-    @Autowired
-    MovementCrudRepository movementCrudRepository;
+        @Autowired
+        MovementCrudRepository movementCrudRepository;
 
-    @Autowired
-    StockRepository stockRepository;
+        @Autowired
+        StockRepository stockRepository;
 
-    @Override
-    public Mono<MovementEntity> addAcquisitionEntryMovement(
-            Long productId, LocalDateTime dateTime, String reason,
-            String comment, int quantity, Long supplierId, Long toLocationId,
-            String transactionSubtype,
-            double transactionValue, Long transactionCurrencyId) {
-        return AddAcquisitionEntryMovement.execute(
-                movementCrudRepository, stockRepository,
-                productId, dateTime, reason,
-                comment, quantity, supplierId, toLocationId,
-                transactionSubtype, transactionValue, transactionCurrencyId);
-    }
+        @Override
+        public Mono<MovementEntity> addAcquisitionEntryMovement(
+                        Long productId, LocalDateTime dateTime, String reason,
+                        String comment, int quantity, Long supplierId, Long toLocationId,
+                        String transactionSubtype,
+                        double transactionValue, Long transactionCurrencyId) {
+                return AddAcquisitionEntryMovement.execute(
+                                movementCrudRepository, stockRepository,
+                                productId, dateTime, reason,
+                                comment, quantity, supplierId, toLocationId,
+                                transactionSubtype, transactionValue, transactionCurrencyId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addCustomerReturnEntryMovement(
-            Long productId, LocalDateTime dateTime, String reason,
-            String comment, int quantity, Long toLocationId, String transactionSubtype,
-            double transactionValue, Long transactionCurrencyId) {
-        return AddCustomerReturnEntryMovement.execute(
-                movementCrudRepository, stockRepository,
-                productId, dateTime, reason,
-                comment, quantity, toLocationId,
-                transactionSubtype, transactionValue, transactionCurrencyId);
-    }
+        @Override
+        public Mono<MovementEntity> addCustomerReturnEntryMovement(
+                        Long productId, LocalDateTime dateTime, String reason,
+                        String comment, int quantity, Long toLocationId, String transactionSubtype,
+                        double transactionValue, Long transactionCurrencyId) {
+                return AddCustomerReturnEntryMovement.execute(
+                                movementCrudRepository, stockRepository,
+                                productId, dateTime, reason,
+                                comment, quantity, toLocationId,
+                                transactionSubtype, transactionValue, transactionCurrencyId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addInventoryAdjustmentEntryMovement(
-            Long productId, LocalDateTime dateTime,
-            String reason, String comment, int quantity, Long toLocationId) {
-        return AddInventoryAdjustmentEntryMovement.execute(
-                movementCrudRepository, stockRepository, productId,
-                dateTime, reason, comment, quantity, toLocationId);
-    }
+        @Override
+        public Mono<MovementEntity> addInventoryAdjustmentEntryMovement(
+                        Long productId, LocalDateTime dateTime,
+                        String reason, String comment, int quantity, Long toLocationId) {
+                return AddInventoryAdjustmentEntryMovement.execute(
+                                movementCrudRepository, stockRepository, productId,
+                                dateTime, reason, comment, quantity, toLocationId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addProductionEntryMovement(
-            Long productId, LocalDateTime dateTime, String reason,
-            String comment, int quantity, Long toLocationId, String transactionSubtype,
-            double transactionValue, Long transactionCurrencyId) {
-        return AddProductionEntryMovement.execute(
-                movementCrudRepository, stockRepository,
-                productId, dateTime, reason,
-                comment, quantity, toLocationId,
-                transactionSubtype, transactionValue, transactionCurrencyId);
-    }
+        @Override
+        public Mono<MovementEntity> addProductionEntryMovement(
+                        Long productId, LocalDateTime dateTime, String reason,
+                        String comment, int quantity, Long toLocationId, String transactionSubtype,
+                        double transactionValue, Long transactionCurrencyId) {
+                return AddProductionEntryMovement.execute(
+                                movementCrudRepository, stockRepository,
+                                productId, dateTime, reason,
+                                comment, quantity, toLocationId,
+                                transactionSubtype, transactionValue, transactionCurrencyId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addSalesOutputMovement(
-            Long productId, LocalDateTime dateTime,
-            String reason, String comment, int quantity,
-            Long fromLocationId, String transactionSubtype,
-            double transactionValue, Long transactionCurrencyId) {
-        return AddSalesOutputMovement.execute(
-                movementCrudRepository, stockRepository,
-                productId, dateTime, reason, comment,
-                quantity, fromLocationId, transactionSubtype,
-                transactionValue, transactionCurrencyId);
-    }
+        @Override
+        public Mono<MovementEntity> addSalesOutputMovement(
+                        Long productId, LocalDateTime dateTime,
+                        String reason, String comment, int quantity,
+                        Long fromLocationId, String transactionSubtype,
+                        double transactionValue, Long transactionCurrencyId) {
+                return AddSalesOutputMovement.execute(
+                                movementCrudRepository, stockRepository,
+                                productId, dateTime, reason, comment,
+                                quantity, fromLocationId, transactionSubtype,
+                                transactionValue, transactionCurrencyId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addSupplierReturnOutputMovement(
-            Long productId, LocalDateTime dateTime, String reason,
-            String comment, int quantity, Long supplierId,
-            Long fromLocationId, String transactionSubtype,
-            double transactionValue, Long transactionCurrencyId) {
-        return AddSupplierReturnOutputMovement.execute(
-                movementCrudRepository, stockRepository,
-                productId, dateTime, reason, comment, quantity,
-                supplierId, fromLocationId, transactionSubtype,
-                transactionValue, transactionCurrencyId);
-    }
+        @Override
+        public Mono<MovementEntity> addSupplierReturnOutputMovement(
+                        Long productId, LocalDateTime dateTime, String reason,
+                        String comment, int quantity, Long supplierId,
+                        Long fromLocationId, String transactionSubtype,
+                        double transactionValue, Long transactionCurrencyId) {
+                return AddSupplierReturnOutputMovement.execute(
+                                movementCrudRepository, stockRepository,
+                                productId, dateTime, reason, comment, quantity,
+                                supplierId, fromLocationId, transactionSubtype,
+                                transactionValue, transactionCurrencyId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addInventoryAdjustmentOutputMovement(
-            Long productId, LocalDateTime dateTime,
-            String reason, String comment, int quantity, Long fromLocationId) {
-        return AddInventoryAdjustmentOutputMovement.execute(
-                movementCrudRepository, stockRepository, productId,
-                dateTime, reason, comment, quantity, fromLocationId);
-    }
+        @Override
+        public Mono<MovementEntity> addInventoryAdjustmentOutputMovement(
+                        Long productId, LocalDateTime dateTime,
+                        String reason, String comment, int quantity, Long fromLocationId) {
+                return AddInventoryAdjustmentOutputMovement.execute(
+                                movementCrudRepository, stockRepository, productId,
+                                dateTime, reason, comment, quantity, fromLocationId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addInternalConsumptionOutputMovement(
-            Long productId, LocalDateTime dateTime,
-            String reason, String comment, int quantity, Long fromLocationId) {
-        return AddInternalConsumptionOutputMovement.execute(
-                movementCrudRepository, stockRepository, productId,
-                dateTime, reason, comment, quantity, fromLocationId);
-    }
+        @Override
+        public Mono<MovementEntity> addInternalConsumptionOutputMovement(
+                        Long productId, LocalDateTime dateTime,
+                        String reason, String comment, int quantity, Long fromLocationId) {
+                return AddInternalConsumptionOutputMovement.execute(
+                                movementCrudRepository, stockRepository, productId,
+                                dateTime, reason, comment, quantity, fromLocationId);
+        }
 
-    @Override
-    public Mono<MovementEntity> addTransferMovement(Long productId,
-            LocalDateTime dateTime, String reason,
-            String comment, int quantity, Long fromLocationId, Long toLocationId) {
-        return AddTransferMovement.execute(
-                movementCrudRepository, stockRepository,
-                productId, dateTime, reason, comment,
-                quantity, fromLocationId, toLocationId);
-    }
+        @Override
+        public Mono<MovementEntity> addTransferMovement(Long productId,
+                        LocalDateTime dateTime, String reason,
+                        String comment, int quantity, Long fromLocationId, Long toLocationId) {
+                return AddTransferMovement.execute(
+                                movementCrudRepository, stockRepository,
+                                productId, dateTime, reason, comment,
+                                quantity, fromLocationId, toLocationId);
+        }
 
-    @Override
-    public Mono<Boolean> cancelMovementById(Long movementId) {
-        return CancelMovementById.execute(movementCrudRepository, stockRepository, movementId);
-    }
+        @Override
+        public Mono<Boolean> cancelMovementById(Long movementId) {
+                return CancelMovementById.execute(movementCrudRepository, stockRepository, movementId);
+        }
+
+        @Override
+        public Flux<StandardMovementDTO> getMovements() {
+                return movementCrudRepository.getMovements();
+        }
+
+        @Override
+        public Flux<EntryMovementDTO> getEntries() {
+                return movementCrudRepository.getEntries();
+        }
+
+        @Override
+        public Flux<OutputMovementDTO> getOutputs() {
+                return movementCrudRepository.getOutputs();
+        }
+
+        @Override
+        public Flux<TransferMovementDTO> getTransfers() {
+                return movementCrudRepository.getTransfers();
+        }
+
+        @Override
+        public Flux<AcquisitionDTO> getAcquisitions() {
+                return movementCrudRepository.getAcquisitions();
+        }
+
+        @Override
+        public Flux<CustomerReturnDTO> getCustomerReturns() {
+                return movementCrudRepository.getCustomerReturns();
+        }
+
+        @Override
+        public Flux<EntryMovementDTO> getEntryInventoryAdjustments() {
+                return movementCrudRepository.getEntryInventoryAdjustments();
+        }
+
+        @Override
+        public Flux<ProductionDTO> getProductions() {
+                return movementCrudRepository.getProductions();
+        }
+
+        @Override
+        public Flux<SaleDTO> getSales() {
+                return movementCrudRepository.getSales();
+        }
+
+        @Override
+        public Flux<SupplierReturnDTO> getSupplierReturns() {
+                return movementCrudRepository.getSupplierReturns();
+        }
+
+        @Override
+        public Flux<OutputMovementDTO> getOutputInventoryAdjustments() {
+                return movementCrudRepository.getOutputInventoryAdjustments();
+        }
+
+        @Override
+        public Flux<OutputMovementDTO> getInternalConsumptionMovements() {
+                return movementCrudRepository.getInternalConsumptionMovements();
+        }
+
+        @Override
+        public Flux<StandardMovementDTO> getMovementsByProductId(Long productId) {
+                return movementCrudRepository.getMovementsByProductId(productId);
+        }
+
+        @Override
+        public Flux<AcquisitionDTO> getAcquisitionsBySupplierId(Long supplierId) {
+                return movementCrudRepository.getAcquisitionsBySupplierId(supplierId);
+        }
+
+        @Override
+        public Flux<AcquisitionDTO> findAcquisitionsByCostAndYear(Long currencyId, double minCost, double maxCost,
+                        int fromYear, int toYear) {
+                return movementCrudRepository.findAcquisitionsByCostAndYear(currencyId, minCost, maxCost,
+                                fromYear, toYear);
+        }
+
+        @Override
+        public Flux<AverageCostProductDTO> getAvgUnitCostByAcquisition(Long productId, Long currencyId, int fromYear,
+                        int toYear) {
+                return movementCrudRepository.getAvgUnitCostByAcquisition(productId, currencyId, fromYear,
+                                toYear);
+        }
+
+        @Override
+        public Flux<AverageCostProductDTO> getAvgTotalCostByAcquisition(Long productId, Long currencyId, int fromYear,
+                        int toYear) {
+                return movementCrudRepository.getAvgTotalCostByAcquisition(productId, currencyId, fromYear,
+                                toYear);
+        }
+
+        @Override
+        public Flux<ProductionDTO> findProductionByCostAndYear(Long currencyId, double minCost, double maxCost,
+                        int fromYear, int toYear) {
+                return movementCrudRepository.findProductionByCostAndYear(currencyId, minCost, maxCost,
+                                fromYear, toYear);
+        }
+
+        @Override
+        public Flux<AverageCostProductDTO> getAvgUnitProductionCost(Long productId, Long currencyId, int fromYear,
+                        int toYear) {
+                return movementCrudRepository.getAvgUnitProductionCost(productId, currencyId, fromYear,
+                                toYear);
+        }
+
+        @Override
+        public Flux<AverageCostProductDTO> getAvgTotalProductionCost(Long productId, Long currencyId, int fromYear,
+                        int toYear) {
+                return movementCrudRepository.getAvgTotalProductionCost(productId, currencyId, fromYear,
+                                toYear);
+        }
+
+        @Override
+        public Flux<SaleDTO> findSalesByValueAndYear(Long currencyId, double minValue, double maxValue, int fromYear,
+                        int toYear) {
+                return movementCrudRepository.findSalesByValueAndYear(currencyId, minValue, maxValue, fromYear,
+                                toYear);
+        }
+
+        @Override
+        public Flux<AverageSellProductDTO> getAvgUnitSellValue(Long productId, Long currencyId, int fromYear,
+                        int toYear) {
+                return movementCrudRepository.getAvgUnitSellValue(productId, currencyId, fromYear,
+                                toYear);
+        }
+
+        @Override
+        public Flux<AverageSellProductDTO> getAvgTotalSellValue(Long productId, Long currencyId, int fromYear,
+                        int toYear) {
+                return movementCrudRepository.getAvgTotalSellValue(productId, currencyId, fromYear,
+                                toYear);
+        }
 
 }
