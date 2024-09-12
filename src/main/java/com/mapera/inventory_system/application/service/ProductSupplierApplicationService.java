8c2c269840e051d45b42ba2928d763606ccac944
MPenunuri <manuel.penunuri.ram@gmail.com>
2024-09-12 10:36:30 -0600

    Implement ProductSupplierApplicationService

---
+++ src/main/java/com/mapera/inventory_system/application/service/ProductSupplierApplicationService.java
@@ -0,0 +1,23 @@
+package com.mapera.inventory_system.application.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mapera.inventory_system.application.port.outbound.ProductSupplierPersistencePort;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductSupplierApplicationService {
+
+    @Autowired
+    private ProductSupplierPersistencePort productSupplierPersistencePort;
+
+    public Mono<Boolean> addProductSupplierRelation(Long productId, Long supplierId) {
+        return productSupplierPersistencePort.addProductSupplierRelation(productId, supplierId);
+    }
+
+    public Mono<Boolean> deleteProductSupplierRelation(Long productId, Long supplierId) {
+        return productSupplierPersistencePort.deleteProductSupplierRelation(productId, supplierId);
+    }
+}
