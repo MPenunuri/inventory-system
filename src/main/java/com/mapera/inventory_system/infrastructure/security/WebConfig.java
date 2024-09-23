c294b2d4f3fb941ed122cd9c777273e74f3c40f4
MPenunuri <manuel.penunuri.ram@gmail.com>
2024-09-23 10:49:54 -0600

    Implemented initial CORS policy configuration

---
+++ src/main/java/com/mapera/inventory_system/infrastructure/security/WebConfig.java
@@ -0,0 +1,21 @@
+package com.mapera.inventory_system.infrastructure.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "https://localhost:8080")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
